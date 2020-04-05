f8d78b2e5a3b5074fd0ad724d96af3599f533f89
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-05 20:57:04 +0100

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+lsblk -o +LABEL,PARTLABEL,UUID,PARTUUID
+DRIVE=/dev/sda
+sgdisk --zap-all $DRIVE
+sgdisk --clear \
+         --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
+         --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:cryptswap \
+         --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
+           $DRIVE
+
+echo "Format EFI Partition"
+
+mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
+
+echo "Encrypt System Partition"
+
+cryptsetup luksFormat --align-payload=8192 -s 256 -c aes-xts-plain64 /dev/disk/by-partlabel/cryptsystem
+cryptsetup open /dev/disk/by-partlabel/cryptsystem system
+
+echo "Bring Up Encrypted Swap"
+
+cryptsetup open --type plain --key-file /dev/urandom /dev/disk/by-partlabel/cryptswap swap
+mkswap -L swap /dev/mapper/swap
+swapon -L swap
