efd3cc5c67bd8d04f827aa3bedb842f9ff27bc3e
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-05 22:59:07 +0100

    Update setup_2.sh

--- setup_2.sh
+++ setup_2.sh
@@ -25,8 +25,10 @@ cp /boot/EFI/refind/refind.conf /boot/EFI/refind/refind.conf.bak
 echo "use_graphics_for windows" >> /boot/EFI/refind/refind.conf
 echo "also_scan_dirs   +,@/             # Search for boot loaders in the specified directory" >> /boot/EFI/refind/refind.conf
 
-
+uid_systema=$(cat /root/uuid_de_system)
+subvolumen_uuid=$(btrfs filesystem show system | grep uuid | awk '{print $4}'
 echo '"Boot with standard options"  "rd.luks.name=*FILL IN UUID FROM PARTITION*=cryptsystem root=UUID=*UUID FROM encrypted root subvolume* rootflags=subvol=root initrd=/intel-ucode.img initrd=/initramfs-linux.img"' >> /boot/refind_linux.conf
+
 
 echo "CUIDADO MIRA EL ULTIMO PASO"
 echo "despues reboot"
