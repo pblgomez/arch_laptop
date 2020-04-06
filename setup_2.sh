0718964db8ce6deec701d7d3a9b55f6c01d1fb14
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-06 21:06:59 +0100

    trying bootloader grub

--- setup_2.sh
+++ setup_2.sh
@@ -24,19 +24,4 @@ echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 btrfs_uuid=$(cat /root/uuid_de_crypto)
 sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID='$btrfs_uuid':system"/' /etc/default/grub
 
-
-
-# pacman -S refind-efi
-# refind-install
-# 
-# cp /boot/EFI/refind/refind.conf /boot/EFI/refind/refind.conf.bak
-# echo "use_graphics_for windows" >> /boot/EFI/refind/refind.conf
-# echo "also_scan_dirs   +,@/             # Search for boot loaders in the specified directory" >> /boot/EFI/refind/refind.conf
-# 
-# uid_systema=$(cat /root/uuid_de_system)
-# subvolumen_uuid=$(btrfs filesystem show system | grep uuid | awk '{print $4}'
-# echo '"Boot with standard options"  "rd.luks.name=*FILL IN UUID FROM PARTITION*=cryptsystem root=UUID=*UUID FROM encrypted root subvolume* rootflags=subvol=root initrd=/intel-ucode.img initrd=/initramfs-linux.img"' >> /boot/refind_linux.conf
-# 
-# 
-# echo "CUIDADO MIRA EL ULTIMO PASO"
-# echo "despues reboot"
+shutdown -P now
