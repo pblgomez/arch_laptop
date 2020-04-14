cfe98814920f0b3e141df110766800dce355266c
Pablo Gómez <pablogomez@pablogomez.com>
2020-04-14 21:50:50 +0100

    cleaning and more variables

--- step1.sh
+++ step1.sh
@@ -15,13 +15,13 @@ sgdisk --zap-all $DRIVE
 if [ $swap = y ]; then
     sgdisk --clear \
          --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
-         --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:cryptswap \
-         --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
+         --new=2:0:+8GiB   --typecode=2:8200 --change-name=2:$name_swap \
+         --new=3:0:0       --typecode=3:8300 --change-name=3:$name_system= \
            $DRIVE
 else
     sgdisk --clear \
          --new=1:0:+550MiB --typecode=1:ef00 --change-name=1:EFI \
-         --new=3:0:0       --typecode=3:8300 --change-name=3:cryptsystem \
+         --new=3:0:0       --typecode=3:8300 --change-name=3:$name_system= \
            $DRIVE
 fi
 
@@ -38,7 +38,7 @@ if [ $swap = y ]; then
     echo "############################################################"
     echo "# Bring Up Encrypted Swap"
     echo "############################################################"
-    cryptsetup open --type plain --key-file /dev/urandom /dev/disk/by-partlabel/cryptswap swap
+    cryptsetup open --type plain --key-file /dev/urandom /dev/disk/by-partlabel/$name_swap swap
     mkswap -L swap /dev/mapper/swap
     swapon -L swap
 fi
@@ -51,63 +51,59 @@ timedatectl set-ntp true
 echo "############################################################"
 echo "# Encrypting system partitions"
 echo "############################################################"
-#cryptsetup luksFormat /dev/disk/by-partlabel/cryptsystem
-cryptsetup luksFormat $encryption /dev/disk/by-partlabel/cryptsystem
-cryptsetup open /dev/disk/by-partlabel/cryptsystem $root_vol_name
+eval cryptsetup luksFormat ${encryption} /dev/disk/by-partlabel/$name_system
+cryptsetup open /dev/disk/by-partlabel/$name_system $root_vol_name
 
 echo "############################################################"
 echo "# Formatting partitions"
 echo "############################################################"
 mkfs.fat -F32 -n EFI /dev/disk/by-partlabel/EFI
-# parted set 1 boot on
-# parted set 1 esp on
 mkfs.btrfs --force --label archroot /dev/mapper/$root_vol_name
 
 echo "############################################################"
 echo "# Mounting partitions"
 echo "############################################################"
-mkdir /mnt/subvolumes
-mount /dev/mapper/$root_vol_name /mnt/subvolumes
+mount /dev/mapper/$root_vol_name /mnt
 
 echo "############################################################"
 echo "# Creating subvolumes"
 echo "############################################################"
-btrfs subvolume create /mnt/subvolumes/home
-btrfs subvolume create /mnt/subvolumes/root
+btrfs subvolume create /mnt/@${distro}
+btrfs subvolume create /mnt/@${distro}_home
+umount /mnt
 
 echo "############################################################"
 echo "# Mounting the subvolumes + boot"
 echo "############################################################"
-o=defaults,x-mount.mkdir
-o_btrfs=$o,compress=lzo,ssd,noatime
-mount -o subvol=root,$o_btrfs /dev/mapper/$root_vol_name /mnt/arch-root
-mkdir /mnt/arch-root/boot
-mount -o subvol=home,$o_btrfs /dev/mapper/$root_vol_name /mnt/arch-root/home
-mount /dev/disk/by-partlabel/EFI /mnt/arch-root/boot
+o_btrfs=defaults,x-mount.mkdir,compress=lzo,ssd,noatime
+mount -o subvol=@${distro},$o_btrfs /dev/mapper/$root_vol_name /mnt/
+mkdir /mnt/boot
+mount -o subvol=@${distro}_home,$o_btrfs /dev/mapper/$root_vol_name /mnt/home
+mount /dev/disk/by-partlabel/EFI /mnt/boot
 
 echo "############################################################"
 echo "# Installing base system"
 echo "############################################################"
-pacstrap /mnt/arch-root base btrfs-progs base-devel refind-efi intel-ucode $kernel \
-    dhcpcd wpa_supplicant wireless_tools iw
+pacstrap /mnt base btrfs-progs base-devel intel-ucode $kernel \
+    efibootmgr grub \
+    dhcpcd wpa_supplicant netctl networkmanager
 
 echo "############################################################"
 echo "# Generating fstab and chroot to new arch system"
 echo "############################################################"
-genfstab -L /mnt/arch-root >> /mnt/arch-root/etc/fstab
+genfstab -L /mnt >> /mnt/etc/fstab
 if [ $swap = y ]; then
-    sed -i s+LABEL=swap+/dev/mapper/swap+ /mnt/arch-root/etc/fstab
-    echo "swap      /dev/disk/by-partlabel/cryptswap        /dev/urandom    swap,offset=2048,cipher=aes-xts-plain64,size=256" >> /mnt/arch-root/etc/crypttab
+    sed -i s+LABEL=swap+/dev/mapper/swap+ /mnt/etc/fstab
+    echo "swap      /dev/disk/by-partlabel/${name_swap}        /dev/urandom    swap,offset=2048,cipher=aes-xts-plain64,size=256" >> /mnt/etc/crypttab
 fi
 
 
 echo "############################################################"
 echo "# Finished step1, now to step2"
 echo "############################################################"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cp $DIR/step2.sh /mnt/arch-root/root/.
-cp $DIR/variables.sh /mnt/arch-root/root/.
-arch-chroot /mnt/arch-root
+cp $DIR/step2.sh /mnt/root/.
+cp $DIR/variables.sh /mnt/root/.
+arch-chroot /mnt
 
 echo "############################################################"
 echo "# Finished step2, now shutdown"
--- variables.sh
+++ variables.sh
@@ -4,8 +4,14 @@ set -e
 ### Variables
 # lsblk -o +LABEL,PARTLABEL,UUID,PARTUUID   # To check what device
 DRIVE=/dev/sda
-root_vol_name=system
-swap=y # yes(y) or no(n)
+distro="arch"
+root_vol_name="system"                      # nombre de la particion root descifrada"
+swap=y                                      # yes(y) or no(n)
+name_swap="cryptswap"                       # nombre de la particion de swap cifrada
+name_system="cryptosystem"                  # nombre de la particion root cifrada
 kernel="linux-zen"
 encryption="--type luks1 --align-payload=8192 -s 256 -c aes-xts-plain64" # you can leave this blank
+locale="en_US"
+set_hostname="archlinux"
+bootloader="grub"                           # grub,refind
 ### End of Variables
