c7fcc469e9dccffd339d2f19b57e60ae667d1ebe
Pablo <pablogomez@pablogomez.com>
2020-04-27 22:38:13 +0100

    Pro Audio and more apps

--- post/300-audio.sh
+++ post/300-audio.sh
@@ -3,8 +3,10 @@ set -e
 
 # pulseaudio, pamixer
 
-echo "----------------------------------------------------------------------"
-echo "     Configuring pulseaudio"
-echo "----------------------------------------------------------------------"
-sudo sed -i 's/^autospawn = no/; autospawn = no/' /etc/pulse/client.conf
-sudo sed -i 's/; autospawn = yes/autospawn = yes/' /etc/pulse/client.conf
+if ! hash pulseaudio 2>/dev/null; then
+  echo "----------------------------------------------------------------------"
+  echo "     Configuring pulseaudio"
+  echo "----------------------------------------------------------------------"
+  sudo sed -i 's/^autospawn = no/; autospawn = no/' /etc/pulse/client.conf
+  sudo sed -i 's/; autospawn = yes/autospawn = yes/' /etc/pulse/client.conf
+fi
