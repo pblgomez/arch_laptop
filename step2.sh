8550563cc08ce5ed81213b919896842517037658
Pablo Gómez <pablogomez@pablogomez.com>
2020-09-18 18:08:54 +0100

    service ntp

--- step2.sh
+++ step2.sh
@@ -46,7 +46,7 @@ echo "127.0.0.1       localhost\n
 systemctl enable NetworkManager
 systemctl enable dhcpcd
 systemctl enable sshd
-systemctl enable ntp
+systemctl enable ntpd
 
 echo "############################################################"
 echo "# mkinitcpio"
